1273c14d1891ed40aaef1355231536e23f996464
JoverZhang <z1181820838@outlook.com>
2018-10-06 05:55:06 +0800

    add 'work commit' page

--- pages/assignments/assignments.js
+++ pages/assignments/assignments.js
@@ -1,11 +1,63 @@
 // pages/assignments/assignments.js
+const { $Message } = require('../../components/iview/dist/base/index');
+const { $Toast } = require('../../components/iview/dist/base/index');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    isSubmit: false,
+    warn: "",
+    content: "",
+  },
 
+  formSubmit: function (e) {
+    // console.log('form发生了submit事件，携带数据为：', e.detail.value);
+    let { content } = e.detail.value;
+    if (!content) {
+      this.setData({
+        warn: "作业内容不能为空！",
+        // isSubmit: true
+      })
+      // console.log('empty'),
+      this.handleError()
+      return;
+    }
+    this.setData({
+      isSubmit: true,
+      warn: "",
+      content: content
+    })
+
+    //提示上传成功
+    this.handleSuccess()
+    setTimeout(() => {
+      wx.navigateBack({
+        delta: 1
+      });
+    }, 3000);
+    // console.log('one', this.data.isSubmit, 'two', this.data.warn, 'three', this.data.content);
+  },
+
+  handleSuccess() {
+    $Toast({
+      content: '作业发布成功',
+      type: 'success',
+      selector: '#pop_toast'
+    });
+  },
+
+  handleError: function () {
+    var self = this;
+    var warning = self.data.warn;
+    // console.log(self.data.warn);
+    $Message({
+      content: warning,
+      type: 'error',
+      selector: '#pop_message',
+      duration: 3
+    });
   },
 
   /**
